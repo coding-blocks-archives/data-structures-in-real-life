ab2c9ece581dd1fb79dd77d77abbccde4d7e12ea
apoorv-x12 <76410421+apoorv-x12@users.noreply.github.com>
2021-07-31 18:10:13 +0530

    Update dom_basic.js

--- JS Introduction/dom_basic.js
+++ JS Introduction/dom_basic.js
@@ -1,6 +1,6 @@
 //Event Listeners
 
-canvas  = document.getElementById("mycanvas");
+canvas  = document.getElementById("mycanvas");// they did global declare but i(apoorv) the one currently reading prefer to declare with let 
 function f(){
 	console.log("You clicked on the canvas");
 }
